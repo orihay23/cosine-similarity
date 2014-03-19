91d970881f6cd01d15076604252877e70492951a
jyahiro <yahiro_john@bah.com>
2014-03-18 23:01:20 -0400

    updating pom and working on cosine-similarity function -seeing some NaN...makes me think normalization is not quite right (divide 0) -have the cosine-similarity matrix built--very sparse

--- src/main/java/cosine/Main.java
+++ src/main/java/cosine/Main.java
@@ -15,114 +15,146 @@ import Jama.Matrix;
 
 public class Main {
 
-	/**
-	 * @param args
-	 * @throws IOException
-	 * @throws ParseException
-	 * @throws BiffException
-	 */
+  /**
+   * @param args
+   * @throws IOException
+   * @throws ParseException
+   * @throws BiffException
+   */
 
-	/*
-	 * ArrayList<String> dupList = ParseUtility.findDuplicateFileNames(new
-	 * ArrayList<String>(), new HashMap<String, String>(), new
-	 * File("F:\\SRWNM 1.0.2")); if(obj.getVulnerabilities().isEmpty()){ } else
-	 * if(obj.getVulnerabilities().size() > 1) { for(String temp :
-	 * obj.getVulnerabilities().keySet()){ System.out.println(temp);
-	 * if(dupList.contains(temp)){ System.out.println("FOUND"); } } } else {}
-	 * System.out.println("this is the number of CU objects created: " +
-	 * sourceObjects.size());
-	 */
+  /*
+   * ArrayList<String> dupList = ParseUtility.findDuplicateFileNames(new
+   * ArrayList<String>(), new HashMap<String, String>(), new
+   * File("F:\\SRWNM 1.0.2")); if(obj.getVulnerabilities().isEmpty()){ } else
+   * if(obj.getVulnerabilities().size() > 1) { for(String temp :
+   * obj.getVulnerabilities().keySet()){ System.out.println(temp);
+   * if(dupList.contains(temp)){ System.out.println("FOUND"); } } } else {}
+   * System.out.println("this is the number of CU objects created: " +
+   * sourceObjects.size());
+   */
 
-	public static void main(String[] args)
-			throws ParseException,
-			IOException,
-			BiffException {
+  public static void main(String[] args)
+      throws ParseException,
+      IOException,
+      BiffException {
 
-		/*generate all class objects*/
-		List<Method> methods =
-				TermExtractor.fromFile(
-						new File(
-                "D:\\Smart_Data\\Behe\\today\\cosine-similarity"));
+    /* generate all class objects */
+    List<Method> methods =
+        TermExtractor.fromFile(
+            new File(
+                "C:\\Users\\John Y\\cw\\cosine-similarity"));
 
-		/*generate master token list with counts as well as each team appearing in a given method*/
-		TreeMap<String, Integer> masterTokenMap =
-				ParseUtility.generateMasterTokenMap();
+    /*
+     * generate master token list with counts as well as each team appearing in
+     * a given method
+     */
+    TreeMap<String, Integer> masterTokenMap =
+        ParseUtility.generateMasterTokenMap();
 
+    /* count number of methods in source code */
+    int count = 0;
+    for (ClassObject co : TermExtractor.sourceObjects) {
+      List<TypeDeclaration> types = co.getCu().getTypes();
+      for (TypeDeclaration type : types) {
+        List<BodyDeclaration> members = type.getMembers();
+        for (BodyDeclaration member : members) {
+          if (member instanceof MethodDeclaration) {
+            if (((MethodDeclaration) member).getMethodTerms() != null) // don't
+                                                                       // count
+                                                                       // methods
+                                                                       // found
+                                                                       // in
+                                                                       // ABSTRACT
+                                                                       // and
+                                                                       // Interface
+                                                                       // classes
+              count++;
+          }
+        }
+      }
+    }
 
-		/*count number of methods in source code*/
-		int count = 0;
-		for (ClassObject co : TermExtractor.sourceObjects) {
-			List<TypeDeclaration> types = co.getCu().getTypes();
-			for (TypeDeclaration type : types) {
-				List<BodyDeclaration> members = type.getMembers();
-				for (BodyDeclaration member : members) {
-					if (member instanceof MethodDeclaration) {
-						if(((MethodDeclaration) member).getMethodTerms() !=null) //don't count methods found in ABSTRACT and Interface classes
-							count++;
-					}
-				}
-			}
-		}
+    /*
+     * set size of function Term Frequency matrix using count from above and
+     * size of the masterTokenMap
+     */
+    Matrix sparseMatrix = new Matrix(count, masterTokenMap.size());
 
-		/*set size of function Term Frequency matrix using count from above and size of the masterTokenMap*/
-		Matrix sparseMatrix = new Matrix(count, masterTokenMap.size());
+    /* generate sparse matrix. */
+    int methodCounter = 0;
+    for (ClassObject co : TermExtractor.sourceObjects) {
+      List<TypeDeclaration> types = co.getCu().getTypes();
+      for (TypeDeclaration type : types) {
+        List<BodyDeclaration> members = type.getMembers();
+        for (BodyDeclaration member : members) {
+          if (member instanceof MethodDeclaration) {
+            int keySetCounter = 0;
+            TreeMap<String, Integer> methodMap =
+                ((MethodDeclaration) member).getMethodTerms();
+            if (methodMap == null) {
+              continue;
+            }
+            for (String keys : masterTokenMap.keySet()) {
 
-		/*generate sparse matrix.  */
-		int methodCounter = 0;
-		for (ClassObject co : TermExtractor.sourceObjects) {
-			List<TypeDeclaration> types = co.getCu().getTypes();
-			for (TypeDeclaration type : types) {
-				List<BodyDeclaration> members = type.getMembers();
-				for (BodyDeclaration member : members) {
-					if (member instanceof MethodDeclaration) {
-						int keySetCounter = 0;
-						TreeMap<String, Integer> methodMap = ((MethodDeclaration) member).getMethodTerms();
-						if(methodMap == null) {continue;}
-						for(String keys : masterTokenMap.keySet()){
+              if (((MethodDeclaration) member).getMethodTerms().containsKey(
+                  keys)) {
+                sparseMatrix.set(
+                    methodCounter,
+                    keySetCounter,
+                    ((MethodDeclaration) member).getMethodTerms().get(keys));
+              }
+              keySetCounter++;
+            }// end of keyset for
+            keySetCounter = 0;
+            methodCounter++;
+          }// end of if
+        }// end of most inner for
+      }// end of second most inner for
+    }// end of outer for
 
-							if(((MethodDeclaration) member).getMethodTerms().containsKey(keys)){
-								sparseMatrix.set(methodCounter, keySetCounter, ((MethodDeclaration) member).getMethodTerms().get(keys));
-							}
-							keySetCounter++;
-						}//end of keyset for
-						keySetCounter = 0;
-						methodCounter++;
-					}//end of if
-				}//end of most inner for
-			}//end of second most inner for
-		}//end of outer for
+    System.out.println("SPARSE MATRIX: "
+        + sparseMatrix.getRowDimension()
+        + " "
+        + sparseMatrix.getColumnDimension());
 
-		System.out.println("SPARSE MATRIX: " + sparseMatrix.getRowDimension() + " " + sparseMatrix.getColumnDimension());
+    /* create IDF matrix */
+    Matrix idf = new Matrix(1, masterTokenMap.size());
+    int keySetCounter = 0;
+    for (String keys : masterTokenMap.keySet()) {
+      // System.out.println(keys);
+      int value = masterTokenMap.get(keys);
+      double finalValue = Math.log(count / (1 + value));
+      idf.set(0, keySetCounter, finalValue);
+      keySetCounter++;
+    }
 
-		
-		/*create IDF matrix*/
-		Matrix idf = new Matrix(1, masterTokenMap.size());
-		int keySetCounter = 0;
-		for(String keys : masterTokenMap.keySet()){
-			//	System.out.println(keys);
-			int value = masterTokenMap.get(keys);
-			double finalValue = Math.log(count/(1+value));
-			idf.set(0, keySetCounter, finalValue);
-			keySetCounter++;
-		}
+    System.out.println("IDF MATRIX: "
+        + idf.getRowDimension()
+        + " "
+        + idf.getColumnDimension());
 
-		System.out.println("IDF MATRIX: " + idf.getRowDimension() + " " + idf.getColumnDimension());
+    /* generate mxm diagonal matrix from the above idf matrix */
+    Matrix idfDiagonal =
+        new Matrix(idf.getColumnDimension(), idf.getColumnDimension());
+    for (int i = 0; i < idf.getColumnDimension(); i++) {
+      idfDiagonal.set(i, i, idf.get(0, i));
+    }
+    System.out.println("DIAG MATRIX: "
+        + idfDiagonal.getRowDimension()
+        + " "
+        + idfDiagonal.getColumnDimension());
 
-		/*generate mxm diagonal matrix from the above idf matrix*/
-		Matrix idfDiagonal = new Matrix(idf.getColumnDimension(), idf.getColumnDimension());
-		for(int i = 0; i<idf.getColumnDimension(); i++){
-			idfDiagonal.set(i, i, idf.get(0, i));
-		}
-		System.out.println("DIAG MATRIX: " + idfDiagonal.getRowDimension() + " " + idfDiagonal.getColumnDimension());
-		
     // result of doing TF * IDF = finalMatrix
-		Matrix finalMatrix = sparseMatrix.times(idfDiagonal);
+    Matrix finalMatrix = sparseMatrix.times(idfDiagonal);
 
-		//finalMatrix.print(1,1);
-		//TODO:  Normalize finalMatrix
+    // finalMatrix.print(1,1);
+    // TODO: Normalize finalMatrix
     // TODO: Cosine Similarity
 
-		System.out.println("FINAL MATRIX: " + finalMatrix.getRowDimension() +  " " +  finalMatrix.getColumnDimension());
+    System.out.println("FINAL MATRIX: "
+        + finalMatrix.getRowDimension()
+        + " "
+        + finalMatrix.getColumnDimension());
 
     /* normalize finalMatrix */
     for (int i = 0; i < finalMatrix.getRowDimension(); i++) {
@@ -133,8 +165,8 @@ public class Main {
         summationValue += Math.pow(summationMatrix.get(0, x), 2);
       }
       double finalSummationValue = Math.sqrt(summationValue);
-      System.out.println("summation Value: " + summationValue);
-      System.out.println("finalSummation Value: " + finalSummationValue);
+      // System.out.println("summation Value: " + summationValue);
+      // System.out.println("finalSummation Value: " + finalSummationValue);
 
       for (int j = 0; j < finalMatrix.getColumnDimension(); j++) {
 
@@ -146,25 +178,58 @@ public class Main {
       }
     }
 
+    /* Cosine Similarity: dot product between each method in training set */
+    Matrix cosineSimilarity =
+        new Matrix(finalMatrix.getRowDimension(), finalMatrix.getRowDimension());
+    for (int i = 0; i < finalMatrix.getRowDimension(); i++) {
+      Matrix firstDot =
+          finalMatrix.getMatrix(i, i, 0, finalMatrix.getRowDimension());
+      double[] firstDotArray = firstDot.getRowPackedCopy();
+      for (int j = 0; j < finalMatrix.getRowDimension(); j++) {
+        Matrix secondDot =
+            finalMatrix.getMatrix(j, j, 0, finalMatrix.getRowDimension());
+        double[] secondDotArray = secondDot.getRowPackedCopy();
+        double dotProduct = dotProduct(firstDotArray, secondDotArray);
+        cosineSimilarity.set(i, j, dotProduct);
+        System.out.println(cosineSimilarity.get(i, j));
+        // System.out.println("Dot Product of row "
+        // + i
+        // + " column "
+        // + j
+        // + ": "
+        // + dotProduct);
+      }
+    }
 
+    //
+    // /* 1. Build Class objects from source */
+    // ArrayList<ClassObject> sourceObjects =
+    // ParseUtility.generateClassObjects(new File("E:\\SRWNM 1.0.2"));
+    // /*
+    // * Map vulnerabilites to classes to include line numbers of
+    // vulnerabillites
+    // * and set to the instance of a ClassObject
+    // */
+    // ParseUtility.generateListOfVulnerabilitiesByClass(
+    // sourceObjects,
+    // new File(
+    // "C:\\Users\\John Y\\workspace\\cosine-similarity\\src\\main\\resources\\NED_20110420_IA_Assessment_SRWNM_V1_0_2.xls"));
+    //
+    // for (ClassObject obj : sourceObjects) {
+    // obj.getCu();
+    // }
+  }
 
-
-		//
-		// /* 1. Build Class objects from source */
-		// ArrayList<ClassObject> sourceObjects =
-		// ParseUtility.generateClassObjects(new File("E:\\SRWNM 1.0.2"));
-		// /*
-		// * Map vulnerabilites to classes to include line numbers of
-		// vulnerabillites
-		// * and set to the instance of a ClassObject
-		// */
-		// ParseUtility.generateListOfVulnerabilitiesByClass(
-		// sourceObjects,
-		// new File(
-		// "C:\\Users\\John Y\\workspace\\cosine-similarity\\src\\main\\resources\\NED_20110420_IA_Assessment_SRWNM_V1_0_2.xls"));
-		//
-		// for (ClassObject obj : sourceObjects) {
-		// obj.getCu();
-		// }
-	}
+  private static double dotProduct(
+      double[] firstDotArray,
+      double[] secondDotArray) {
+    double sum = 0;
+    for (int j = 0; j < firstDotArray.length; j++) {
+      sum += firstDotArray[j] * secondDotArray[j];
+      // System.out.println("first " + firstDotArray[j]);
+      // System.out.println("2nd " + secondDotArray[j]);
+      // System.out.println("sum is: " + sum);
+    }
+    return sum;
+  }
 }
